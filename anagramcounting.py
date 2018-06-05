1f220addd5f702f7d495a46f146cd6019f9e172e
danfloyd111 <dan.floyd111@gmail.com>
2018-06-06 00:05:43 +0200

    Solution for Anagram Counting problem

---
+++ anagramcounting.py
@@ -0,0 +1,18 @@
+import sys, math
+
+for line in sys.stdin:
+
+    char_map = {}
+    line = line[0:len(line)-1] # removing new-line
+    
+    for char in line:
+        if char in char_map:
+            char_map[char] += 1
+        else:
+            char_map[char] = 1
+    
+    result = math.factorial(len(line))
+    for key, val in char_map.items():
+        result //= math.factorial(val)
+    
+    print(result)

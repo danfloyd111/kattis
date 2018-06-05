64c7846472ca85effa7c78392bea0d02125aea32
danfloyd111 <dan.floyd111@gmail.com>
2018-06-06 00:23:12 +0200

    Solution for Apaxiaaans problem

---
+++ apaxiaaans.py
@@ -0,0 +1,14 @@
+import sys
+
+name = sys.stdin.readline()
+name = name[0:len(name)-1] # removing new-line
+
+last_char = ''
+short_name = ""
+
+for char in name:
+    if char != last_char:
+        short_name += char
+    last_char = char
+
+print(short_name)

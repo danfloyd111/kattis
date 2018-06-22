0e8581a063b1bece3e1bf3d739e597c0b29ec224
Daniele Paolini <dan.floyd111@gmail.com>
2018-06-22 18:12:55 +0200

    Solution for Paradox With Averages problem
    I understood badly the problem, now works

--- averageseasy.py
+++ averageseasy.py
@@ -4,12 +4,6 @@ import sys
 def get_values(ls):
     return list(map(lambda x: int(x), ls))
 
-# avg of a list of values
-def avg(ls):
-    if len(ls) == 0:
-        return None
-    return sum(ls) / len(ls)
-
 t = int(sys.stdin.readline())
 
 for i in range(t):
@@ -17,18 +11,28 @@ for i in range(t):
     sys.stdin.readline() # empty line before test case
     
     tokens = sys.stdin.readline().split() # actually useless input
-    
+    n_cs = int(tokens[0])
+    n_e = int(tokens[1])
+
     n_s = 0 # number of students to transfer
 
     cs_values = sorted(get_values(sys.stdin.readline().split()), reverse=True)
     e_values = get_values(sys.stdin.readline().split()) # don't mind to sort values of economics students
 
-    # actually sums of weights are the averages
-    cs_avg = avg(cs_values)  
-    e_avg = avg(e_values)
+    cs_tot = sum(cs_values)
+    e_tot = sum(e_values)
+    cs_avg = cs_tot / n_cs  
+    e_avg = e_tot / n_e
 
-    while ( avg(cs_values) <= cs_avg and avg(e_values) <= e_avg ):
-        e_values.append(cs_values.pop()) # move a student from computer science to economics
-        n_s += 1
+    h = 0
+
+    if (e_avg < cs_avg):
+        while (h < n_cs):
+            # test every computer science student
+            temp_cs_avg = (cs_tot - cs_values[h]) / (n_cs - 1)
+            temp_e_avg = (e_tot + cs_values[h]) / (n_e + 1)
+            if (temp_cs_avg > cs_avg and temp_e_avg > e_avg):
+                n_s += 1
+            h += 1 
 
     print(n_s)

27892a3f40a25caaa5212b6c98c1cec241cd8077
danfloyd111 <dan.floyd111@gmail.com>
2018-06-23 22:14:34 +0200

    Solution for Character problem

---
+++ character.py
@@ -0,0 +1,17 @@
+import sys
+from math import factorial
+
+def binomial(n, k):
+    return factorial(n) / (factorial(n - k) * factorial(k)) 
+
+n = int(sys.stdin.readline())
+result = 0
+
+if (n != 0 and n != 1):
+    i = 2
+    while (i < n):
+        result += binomial(n, i)
+        i += 1
+    result += 1
+
+print(int(result))

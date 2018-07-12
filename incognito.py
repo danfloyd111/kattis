e463827672df1112fd92d53f2c6e8a9b7e835d50
Daniele Paolini <d.paolini@stetel.com>
2018-07-12 16:25:25 +0200

    Solution for Incognito problem

---
+++ incognito.py
@@ -0,0 +1,22 @@
+import sys
+
+cases = int(sys.stdin.readline())
+
+for i in range(cases):
+
+    categories = int(sys.stdin.readline())
+    store = { }
+
+    for j in range(categories):
+
+        cat = sys.stdin.readline().split()[1]
+        if cat in store:
+            store[cat] += 1
+        else:
+            store[cat] = 2
+
+    prod = 1
+    for key in store.keys():
+        prod *= store[key]
+
+    print(prod - 1)
